c26333352aca698d55cfa6d1b95ccd3d7b997796
yasin1ar <yasin.amirany@gmail.com>
2022-08-12 00:30:30 +0430

    modified the first commit

--- passG.py
+++ passG.py
@@ -1,4 +1,5 @@
-from random import randint, choices, choice
+from random import choices, choice
+
 class PassGenerator:
 
 	chars = ['1234567890', 'qwertyuiopasdfghjklzxcvbnm', '!@#$%&']
@@ -9,12 +10,15 @@ class PassGenerator:
 		for i in choices(PassGenerator.chars[1],k=3) : password += i ; password = password.title()
 		for i in choices(PassGenerator.chars[0],k=3) : password += i 
 		for i in choices(PassGenerator.chars[2],k=2) : password += i 
+
 		password_list = list(password)
 		password = ''
+
 		while len(password) != 8:
+			
 			char = choice(password_list)
 			if char not in password : password += char
-		assert len(password) == 8, "lengh is not 8"
+
 		return password
 
 print(PassGenerator.create_pass())
