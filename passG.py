a011c525bef39abf194d29150bfdcd6a7218d767
yasin1ar <yasin.amirany@gmail.com>
2022-08-12 03:03:46 +0430

    added some feature

--- passG.py
+++ passG.py
@@ -20,3 +20,50 @@ class PassGenerator:
 			if char not in password : password += char
 
 		return password
+
+class PassManager:
+
+	path = '/home/Yasin10ar/Documents/MyDucs/PassG/passwords.txt'
+
+	def add():
+		with open(PassManager.path, 'a') as f:
+			f.write(f"{profile} : {PassGenerator.create_pass()}\n")
+
+
+	def delete():
+		with open(PassManager.path, "w") as f:
+			lines = f.readlines()
+			for line in lines:
+				if profile not in line:
+					f.write(line)
+
+	def replace():
+		with open(PassManager.path, "w") as f:
+			lines = f.readlines()
+			for line in lines:
+				if profile in line:
+					print(line)
+
+	def main():
+		with open(PassManager.path, 'r') as f:
+			lines = f.readlines()
+			print(lines)
+			if profile in lines:
+				option = input(f"{profile} already exist, what do you want to do? (delete/replace) : ")
+
+				if option == 'delete':
+					PassManager.delete()
+				elif option == 'replace':
+					pass
+				else:
+					print("wrong input, options are 'delete' and 'replace'/n exiting")
+
+			else:
+				PassManager.add()
+
+
+if __name__ == "__main__":
+	
+	profile =input('PassG > ')
+	PassManager.main()
+	exit()
