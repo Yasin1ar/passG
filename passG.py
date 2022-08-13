89eb23072684451bc80ddac560da2a916a85ead8
Yasin1ar <yasin.amirany@gmail.com>
2022-08-13 21:36:39 +0430

    one step close to heaven

--- passG.py
+++ passG.py
@@ -1,11 +1,12 @@
-""" A program that generates Solid 8-digits passwords, and keep them safe in .txt file in the chosen folder """
+""" A program that generates Solid 8-digits passwords, and keep them safe in .txt file in the chosen folder,
+ and sends update via telegram """
 from random import choices, choice
 
 class PassGenerator:
 
 	chars = ['1234567890', 'qwertyuiopasdfghjklzxcvbnm', '!@#$%&']
 
-	def create_pass():
+	def create_pass() -> str:
 
 		password = ''
 		for i in choices(PassGenerator.chars[1],k=3) : password += i ; password = password.title()
@@ -25,56 +26,68 @@ class PassGenerator:
 					password += l
 
 		return password
-		
+
 class PassManager:
 
 	file = "C:\\Users\\yasin\\Documents\\MyDucs\\Important\\passwords\\Passwords.txt"
 
 	def add():
 		with open(PassManager.file, 'a') as f:
-			f.write(f"{profile} : {PassGenerator.create_pass()}\n")
-			exit()
-
+			password = PassGenerator.create_pass()
+			f.write(f"{profile} : {password}\n")
+			print(f"{profile} : {password}\n")
 
 	def delete():
-		with open(PassManager.file, "w") as f:
+		with open(PassManager.file, "r") as f:
 			lines = f.readlines()
-			for line in lines:
-				if profile not in line:
+			lines.remove(line_to_remove)
+			with open(PassManager.file, 'w') as f:
+				for line in lines:
 					f.write(line)
 
+		print(f"{profile}'ve been successfully deleted ")
+
 	def replace():
-		with open(PassManager.file, "w") as f:
+		with open(PassManager.file, "r") as f:
 			lines = f.readlines()
-			for line in lines:
-				if profile in line:
-					print(line)
+			with open(PassManager.file, 'w') as f:
+				for line in lines:
+					if profile in line.split(":")[0]:
+						new_password = PassGenerator.create_pass()
+						line = f"{profile} : {new_password}\n"
+					f.write(line)
+
+		print("successfully replaced the previous password with new one({})".format(new_password))
 
 	def reorder():
 		with open(PassManager.file, 'r+') as f:
 			lines = f.readlines()
 
-
 class Main:
 
 	def main():
 
-		with open(PassManager.file, 'r') as f:
-			lines = f.readlines()
+		global profile
+		profile = ''
 
-			global profile
-			profile = 'show'
-			commands = ['show', 'delete all', 'help', 'Help', 'HELP',' passG', 'PassG']
-			while profile in commands or len(profile) < 4:
+		commands = ['show', 'delete all', 'help', 'Help', 'HELP', 'passG', 'PassG', 'passg']
+		while profile in commands or len(profile) < 4:
+
+			with open(PassManager.file, 'r') as f:
+				lines = f.readlines()
 
 				profile = input(">> ")
 
 				if profile == "show":
-					list=[]        
-					for i in lines:
-						l = i.split(':')
-						line=f"{l[0].strip()} : {l[1][0:-1]}"
-						list.append(line)
+					if len(lines) >= 1:
+						list = []
+						for i in lines:
+							l = i.split(':')
+							line=f"{l[0].strip()} : {l[1][0:-1]}"
+							list.append(line)
+					else:
+						print("The file is empty")
+						continue	
 
 					print(list)
 					
@@ -83,28 +96,42 @@ class Main:
 						f.write('')
 						exit()
 
-				elif profile in commands[2:-1]:
+				elif profile in commands[2:]:
 					print(" Commands are 'show' and 'delete all' ")
+					print("hint : strongly recommend to use a good and memorable or informational profile \
+					that will help you to know which password is for which , \
+					e.x of profile [ Yasin10ar@gmail.com , facebook.com , Crypto Wallet & ...] ")
+
+				elif len(profile) < 4 :
+					print('If you want to generate a password, you must enter a word with more than 3 character')
+					print("type 'help' for more\n")
 
 
-			if len(lines) == 0:PassManager.add()
+		with open(PassManager.file, 'r+') as f:
+			lines = f.readlines()
+			if len(lines) == 0 : PassManager.add() ; exit()
+
 			for i in lines:
 				l = i.split(":")
 
 				if profile in l[0]:
 					option = input(f"{profile} already exist, what do you want to do? (delete/replace) : ")
+					
 
 					if option == 'delete':
+						global line_to_remove
+						line_to_remove = i
 						PassManager.delete()
+						break
 					elif option == 'replace':
 						PassManager.replace()
+						break
 					else:
 						print("wrong input, options are 'delete' and 'replace'/n exiting ...")
 
 				else:
 					PassManager.add()
-
-
+					break
 
 if __name__ == "__main__":
 	Main.main()
--- test.py
+++ test.py
@@ -5,12 +5,13 @@ from passG import PassGenerator
 class Test_pass_generator(TestCase):
 
     def test_len(self):
+        for i in range(50):
             self.assertEqual(len(PassGenerator.create_pass()), 8)
 
-    def test_raise_error(self):
-            self.assertRaises(AssertionError, PassGenerator.create_pass(), )
+    def test_type(self):
+        self.assertEqual(type(PassGenerator.create_pass()),str)
+
 
 
 if __name__ == '__main__':
-    for i in range(5):
-        unittest.main()
+    unittest.main()
